836662d1944afc1997c33b407ab1cef119fd3c8d
nishitaa <nishita_durapid@outlook.com>
2023-06-28 10:47:29 +0530

    created env file

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ const App = () => {
           <Routes>
             <Route path="/" exact element={<Feed />} />
             <Route path="/video/:id" exact element={<VideoDetail />} />
-            <Route path="/video/:id" exact element={<VideoDetail />} />
             <Route path="/channel/:id" exact element={<ChannelDetail />} />
             <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
           </Routes>
