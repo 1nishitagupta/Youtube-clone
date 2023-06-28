11c7c592db9c3c8323f8cbd18acabd3149f13b22
nishitaa <nishita_durapid@outlook.com>
2023-06-28 10:43:50 +0530

    created env

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+
+  useEffect(() => {
+    fetchFromAPI(`channel?id=${id}`).then((data) => {
+      setChannelDetail(data?.meta);
+    });
+  }, [id]);
+  console.log(channelDetail);
   return <div>ChannelDetail</div>;
 };
 
